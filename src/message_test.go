40779224f5f6a29628894c44bfdeadf937fdb3df
mick-roper <mick.roper@dazn.com>
2018-08-23 10:10:05 +0100

    started adding tests

---
+++ src/message_test.go
@@ -0,0 +1,46 @@
+package gomq
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_NewMessage(t *testing.T) {
+	type args struct {
+		payload []byte
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		expect  *Message
+		wantErr bool
+	}{
+		{
+			name:    "creates valid message",
+			args:    args{[]byte("hello, world")},
+			wantErr: false,
+			expect:  &Message{make(map[string]string, 0), []byte("hello, world")},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			msg, err := NewMessage(test.args.payload)
+
+			if test.wantErr != (err != nil) {
+				if err != nil {
+					t.Error(err)
+				} else {
+					t.Error("expected an error")
+				}
+
+				return
+			}
+
+			if !reflect.DeepEqual(msg, test.expect) {
+				t.Errorf("expected %v to be %v", msg, test.expect)
+			}
+		})
+	}
+}
